baee61af7eac2cc92ee6a38f14018a9af58a0c5f
Kar Hoe Leong <karh.leong@gmail.com>
2025-04-14 16:50:44 +0800

    footprint icon

--- src/components/Homepage/Comm_Stats.jsx
+++ src/components/Homepage/Comm_Stats.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Footprints } from 'lucide-react';
 import './Comm_Stats.css';
 
 function Stats({ totalCarbonEmission }) {
   // Formatting the carbon emission value
   const formattedCarbon = totalCarbonEmission ? Math.round(totalCarbonEmission * 1000).toLocaleString() : '0';
 
-
   return (
     <section className="stats-highlight">
       <div className="stats-text">
         <h2>Carbon Footprint Reduced by the Community</h2>
-        <div className="stat-number">{formattedCarbon}</div>
+        <div className="stat-number-container">
+          <div className="stat-number">{formattedCarbon}</div>
+          <Footprints className="footprint-icon" size={50} color="seagreen" />    
+        </div>
         <p><strong className="emphasized-text">g·CO₂e</strong></p>
+        
         <p>
           What started as a small effort to reduce waste has
           blossomed into a <strong>community-wide movement</strong>, with
